407ace27f74f8fa360e61a0a8646649ec25fdc38
DeeJay <1018805743@qq.com>
2017-09-14 14:11:14 +0800

    import jquery

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import $ from 'jquery';
 function componet() {
-    var element = document.createElement('div');
+    var element = $('<div></div>');
 
-    element.innerHTML = _.join(['hello,webpack'],'');
+    element.html(_.join(['hello','webpack'],''));
     // _.join()中的_是lodash暴露的全局变量
-    return element;
+
+    return element.get(0);
 }
 document.body.appendChild(componet());
