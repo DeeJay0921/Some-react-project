834b0fa9f78c6540423e5e8d5b6ef07054c37c25
DeeJay <1018805743@qq.com>
2017-09-14 21:15:10 +0800

    react-demo

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Welcome from './welcome';
 
 ReactDOM.render(
-    <h1>hi,deejay</h1>,
-    document.getElementById('root2')
+    <Welcome name="deejay"/>,
+    document.getElementById('root')
 );
---
+++ src/welcome.js
@@ -0,0 +1,25 @@
+import React from 'react';
+class Welcome extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+        setInterval(() => {
+            this.setState({
+                date: new Date()
+            })
+        })
+    }
+    render () {
+        return (
+            <div>
+                <h1>hello, {this.props.name}</h1>
+                <h2>{this.state.date.toString()}</h2>
+            </div>
+        )
+    }
+}
+
+export default Welcome;
+
