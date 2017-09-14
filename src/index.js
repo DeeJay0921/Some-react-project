68ff3cc15704881fa59e25d03b301264e92b57ce
DeeJay <1018805743@qq.com>
2017-09-14 22:16:41 +0800

    basic framework

--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Welcome from './welcome';
 
-ReactDOM.render(
-    <Welcome name="deejay"/>,
-    document.getElementById('root')
-);
